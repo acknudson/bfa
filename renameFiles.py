43226d4006fe50cac8ed0f27aab096309967c287
Aubrey Colter <ajcolter@gmail.com>
2016-01-27 00:46:12 -0500

    Resized all of the images I downloaded. Wrote the script to print out the image names. Added them to final.html, which looks like it could be a final version of the project.

--- renameFiles.py
+++ renameFiles.py
@@ -1,2 +1,12 @@
-import os
+from os import listdir
+from os.path import isfile, join
+import sys
 
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+
+mypath = sys.argv[1]
+
+fileList = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+print fileList
